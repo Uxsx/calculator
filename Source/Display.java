7160613b5a86bfa18743446cb816c93eb7deaab0
Mackeyth28 <thapkrncingam@gmail.com>
2026-02-05 21:18:11 +0700

    display

--- Source/Display.java
+++ Source/Display.java
@@ -1,5 +1,11 @@
-package Source;
-
 public class Display {
-    
+   public Display() {
+      System.out.println("______________");
+      System.out.println("| 7, 8, 9, / |");
+      System.out.println("| 4, 5, 6, * |");
+      System.out.println("| 1, 2, 3, - |");
+      System.out.println("| 0, %, =, + |");
+      System.out.println("______________");
+      System.out.println("");
+   }
 }
